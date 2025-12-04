cefe7545bc4f21c47ed46cd3f10b2f99da382923
phiphipiupiu <priverco5614@gmail.com>
2025-12-04 09:26:05 +0800

    Added admin page components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
+import Menu from "@/components/Menu";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -9,13 +11,17 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/* LEFT */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
       <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
       <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
       <span className="hidden lg:block">School Lama</span>
       </Link>
+      <Menu />
     </div>
     {/* RIGHT */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">right</div>
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar/>
+      {children}
+    </div>
   </div>;
 }
---
+++ src/components/CountChart.tsx
@@ -0,0 +1,79 @@
+"use client"
+import Image from 'next/image';
+import { RadialBarChart, RadialBar, Legend } from 'recharts';
+
+// #region Sample data
+const data = [
+    {
+        name: '18-24',
+        uv: 31.47,
+        pv: 2400,
+        fill: '#8884d8',
+    },
+    {
+        name: '25-29',
+        uv: 26.69,
+        pv: 4567,
+        fill: '#83a6ed',
+    },
+    {
+        name: '30-34',
+        uv: 15.69,
+        pv: 1398,
+        fill: '#8dd1e1',
+    },
+    {
+        name: '35-39',
+        uv: 8.22,
+        pv: 9800,
+        fill: '#82ca9d',
+    },
+    {
+        name: '40-49',
+        uv: 8.63,
+        pv: 3908,
+        fill: '#a4de6c',
+    },
+    {
+        name: '50+',
+        uv: 2.63,
+        pv: 4800,
+        fill: '#d0ed57',
+    },
+    {
+        name: 'unknown',
+        uv: 6.67,
+        pv: 4800,
+        fill: '#ffc658',
+    },
+];
+
+
+const CountChart = () => {
+    return (
+        <div className='bg-white rounded-xl w-full h-full p-4'>
+            {/* TITLE */}
+            <div className='flex justify-between items-center'>
+                <h1>Students</h1>
+                <Image src="/moreDark.png" alt="" width={20} height={20}/>
+            </div>
+            {/* CHART */}
+            <div className=''>
+                {/* <RadialBarChart
+                    style={{ width: '100%', maxWidth: '700px', maxHeight: '80vh', aspectRatio: 1.618 }}
+                    responsive
+                    cx="30%"
+                    barSize={14}
+                    data={data}
+                >
+                    <RadialBar label={{ position: 'insideStart', fill: '#fff' }} background dataKey="uv" />
+                    <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
+                </RadialBarChart> */}
+            </div>
+            {/* BOTTOM */}
+            <div className=''></div>
+        </div>
+    )
+}
+
+export default CountChart
