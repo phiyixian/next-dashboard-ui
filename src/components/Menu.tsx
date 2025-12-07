c49e182afe629d49dc3c49b7accc7a76df40b0c7
phiphipiupiu <priverco5614@gmail.com>
2025-12-07 20:46:56 +0800

    Added react big calendar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { role } from "@/lib/data";
 
 const menuItems = [
   {
@@ -123,13 +124,15 @@ const Menu = () => {
         <div key={i.title} className="flex flex-col gap-2">
           <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
           {i.items.map( item => (
-            <Link 
+            item.visible.includes(role) ? (
+              <Link 
               href={item.href} 
               key={item.label} 
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md-px-2 rounded-md hover:bg-lamaSkyLight">
               <Image src={item.icon} alt="" width={20} height={20}/>
               <span className="hidden lg:block">{item.label}</span>
             </Link>
+            ) : null
           ))}
         </div>
       ))}
